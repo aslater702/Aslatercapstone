e64fdbf136a674aa9be1997ff6a60aa038b02f18
Aslater702 <audrey.slater@slalom.com>
2021-12-06 16:49:11 -0800

    fixed a million different things

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-import requests
-import json
+from flask import Flask
 
 app = Flask(__name__)
-api = Api(app)
 
 #start of hello world app    
 @app.route('/')
